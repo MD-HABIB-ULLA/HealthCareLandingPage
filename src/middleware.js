ae08c62586d4c55372b4b8544d445d3d0f885ffc
habib ulla <habibulla1278@gmail.com>
2024-07-09 20:02:45 +0600

    middleware added

---
+++ src/middleware.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export default function middleWare(request){
+    return NextResponse.redirect(new URL("/about", request.url))
+}
+export const config = {
+    matcher : "/"
+}
